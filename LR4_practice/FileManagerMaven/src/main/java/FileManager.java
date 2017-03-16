fe366ab98d08908c1be9c6f8346a9a27cbe95c45
romanodessa <romashpiller@gmail.com>
2017-03-16 18:08:17 +0200

    LR4_practice_Maven

---
+++ LR4_practice/FileManagerMaven/src/main/java/FileManager.java
@@ -0,0 +1,137 @@
+import java.io.*;
+
+public class FileManager {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public void createFile() throws IOException {
+        boolean result;
+
+        File file = new File(pathInput());
+        result = file.createNewFile();
+        if (result) {
+            System.out.println("File has been created");
+        } else {
+            System.out.println("File already exists");
+        }
+    }
+
+    public void deleteFile() throws IOException {
+        boolean result;
+
+        File file = new File(pathInput());
+        result = file.delete();
+
+        if (result) {
+            System.out.println("File has been deleted");
+        } else {
+            System.out.println("File hasn't been found or can't be deleted");
+        }
+    }
+
+    public void renameFile() throws IOException {
+        boolean result;
+        String pathNew;
+
+        File file = new File(pathInput());
+        System.out.println("Please, input a path and new file name: ");
+
+        pathNew = reader.readLine();
+        File fileNew = new File(pathNew);
+        result = file.renameTo(fileNew);
+
+        if (result) {
+            System.out.println("File has been renamed");
+        } else {
+            System.out.println("File hasn't been renamed");
+        }
+    }
+
+    public void findWord() throws IOException {
+        String word;
+        int val = 0;
+        String line;
+
+        File file = new File(pathInput());
+
+        System.out.println("Input the word for search: ");
+        word = reader.readLine();
+
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        while ((line = reader.readLine()) != null) {
+            if (line.contains(word)) {
+                System.out.println("The word has been found");
+                val = 1;
+                break;
+            } else {
+                val = 0;
+            }
+        }
+
+        if (val == 0) {
+            System.out.println("The word hasn't been found");
+        }
+    }
+
+    public void replaceWord() throws IOException {
+        String word;
+        String wordNew;
+        String line;
+        String textOld = "";
+        String textNew;
+        String path = pathInput();
+
+        File file = new File(path);
+
+        System.out.println("Input the word that should be replaced: ");
+        word = reader.readLine();
+
+        System.out.println("Input the new word: ");
+        wordNew = reader.readLine();
+
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        while ((line = reader.readLine()) != null) {
+            if (System.getProperty("os.name").contains("Windows")) {
+                textOld += line + "\r\n";
+            } else {
+                textOld += line + "\n";
+            }
+        }
+        reader.close();
+
+        if (textOld.contains(word)) {
+            textNew = textOld.replaceAll(word, wordNew);
+            FileWriter writer = new FileWriter(path);
+            writer.write(textNew);
+            writer.close();
+            System.out.println("The word has been replaced");
+        } else {
+            System.out.println("Such word is missing in this file");
+        }
+    }
+
+    public String pathInput() throws IOException {
+        String os;
+        String path;
+
+        os = System.getProperty("os.name");
+        System.out.println("Your operation system is " + os);
+
+        if (os.contains("Windows")) {
+            System.out.println("Please, input a path and file name in format (example): C:\\Users\\user\\1.txt");
+            path = reader.readLine();
+        } else if (os.contains("Mac OS")) {
+            System.out.println("Please, input a path and file name in format (example) /Users/user/projects/1.txt");
+            path = reader.readLine();
+        } else if (os.contains("Linux")) {
+            System.out.println("Please, input a path and file name in format (example) /home/user/projects/1.txt");
+            path = reader.readLine();
+        } else {
+            System.out.println("Please, input a path and file name");
+            path = reader.readLine();
+        }
+
+        return path;
+    }
+}
---
+++ LR4_practice/FileManagerMaven/src/main/java/Main.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String action;
+
+        FileManager fm = new FileManager();
+
+        System.out.println("Choose an action:");
+        System.out.println("1. Create File");
+        System.out.println("2. Delete File");
+        System.out.println("3. Rename File");
+        System.out.println("4. Find Word");
+        System.out.println("5. Replace Word");
+        System.out.println("0. Exit");
+
+        do {
+            action = reader.readLine();
+            switch (action) {
+                case "0":
+                    break;
+
+                case "1":
+                    fm.createFile();
+                    break;
+
+                case "2":
+                    fm.deleteFile();
+                    break;
+
+                case "3":
+                    fm.renameFile();
+                    break;
+
+                case "4":
+                    fm.findWord();
+                    break;
+
+                case "5":
+                    fm.replaceWord();
+                    break;
+
+                default:
+                    System.out.println("Unacceptable action! Choose an action from the list");
+            }
+        }
+        while (!action.equals("0"));
+    }
+}
